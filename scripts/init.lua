de2d7240dcbd7a1e898dc59dd8f6cf2ae86846a9
Michael Lingelbach <m.j.lbach@gmail.com>
2021-03-20 09:53:47 -0700

    Add minimal init.lua

---
+++ scripts/init.lua
@@ -0,0 +1,40 @@
+local install_path = '/tmp/babelfish/site/pack/packer/opt/packer.nvim'
+
+if vim.fn.empty(fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+-- Only required if you have packer in your `opt` pack
+vim.cmd [[packadd packer.nvim]]
+vim.api.nvim_exec([[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost plugins.lua PackerCompile
+  augroup end
+]], false)
+
+vim.cmd[[set packpath=/tmp/nvim/site]]
+
+local use = require('packer').use
+require('packer').startup(
+  {
+    function()
+  use { 'wbthomason/packer.nvim', opt = true }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  end,
+  config = {package_root = "/tmp/babelfish/site/pack"},
+}
+)
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+parser_config.markdown = {
+  install_info = {
+    url = "https://github.com/ikatyang/tree-sitter-markdown",
+    files = { "src/parser.c", "src/scanner.cc" },
+  }
+}
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "markdown" }
+}
