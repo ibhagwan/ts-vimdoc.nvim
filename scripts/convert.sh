5360a6d45fc47459dd446d7b91ff6924209abc5d
Michael Lingelbach <m.j.lbach@gmail.com>
2021-03-20 10:24:35 -0700

    Workaround async stuff

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,1 +1,5 @@
-nvim -c ":luafile main.lua" -c ":q"  --headless
+#!/usr/bin/env bash
+# nvim -u scripts/init.lua -c ":TSInstallSync markdown" -c ":q" --headless
+nvim -u scripts/init.lua -c ":PackerSync" -c ":sleep 20" -c ":q" --headless
+nvim -u scripts/init.lua -c ":sleep 20" -c ":q"  --headless
+nvim -u scripts/init.lua -c ":luafile main.lua" -c ":q"  --headless
--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,6 @@
 local install_path = '/tmp/babelfish/site/pack/packer/opt/packer.nvim'
 
-if vim.fn.empty(fn.glob(install_path)) > 0 then
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
@@ -13,7 +13,7 @@ vim.api.nvim_exec([[
   augroup end
 ]], false)
 
-vim.cmd[[set packpath=/tmp/nvim/site]]
+vim.cmd[[set packpath=/tmp/babelfish/site]]
 
 local use = require('packer').use
 require('packer').startup(
@@ -25,16 +25,17 @@ require('packer').startup(
   config = {package_root = "/tmp/babelfish/site/pack"},
 }
 )
+if pcall(require, "nvim-treesitter.parsers") then
+  local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-
-parser_config.markdown = {
-  install_info = {
-    url = "https://github.com/ikatyang/tree-sitter-markdown",
-    files = { "src/parser.c", "src/scanner.cc" },
+  parser_config.markdown = {
+    install_info = {
+      url = "https://github.com/ikatyang/tree-sitter-markdown",
+      files = { "src/parser.c", "src/scanner.cc" },
+    }
   }
-}
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "markdown" }
-}
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "markdown" }
+  }
+end
