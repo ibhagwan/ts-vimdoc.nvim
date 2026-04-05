592a99bb0911cfad685c4ab16de78489d48f7beb
phanium <91544758+phanen@users.noreply.github.com>
2026-04-05 19:52:51 +0800

    chore: remove nvim-treesitter
    nvim is bundled with basic markdown's parsers/queries

--- scripts/init.lua
+++ scripts/init.lua
@@ -4,9 +4,3 @@ vim.g.loaded_netrwPlugin = 1
 local tempdir = vim.trim(vim.fn.system('sh -c "dirname $(mktemp -u)"'))
 local packpath = os.getenv("PACKPATH") or tempdir .. "/ts-vimdoc.nvim.tmp/nvim/site"
 vim.cmd("set packpath=" .. packpath)
-
-require "nvim-treesitter".setup {}
-require "nvim-treesitter".install {
-  "markdown",
-  "markdown_inline",
-}
